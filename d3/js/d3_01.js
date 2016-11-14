fdce236b91e08e7fda8367ee196995b0f57a3db4
chenyukk <497736396@qq.com>
2016-11-14 15:02:34 +0800

    20131114b

--- d3/js/d3_01.js
+++ d3/js/d3_01.js
@@ -1,4 +1,25 @@
-var mysvg=d3.slect('svg');
-var p1= mysvg('circle');
-p1.attr('')
+var output = d3.select('#op');
+function randomColor(){
+        var color1,i,j,k;
+        i= Math.floor(Math.random()*256).toString(16);
+        j= Math.floor(Math.random()*256).toString(16);
+        k= Math.floor(Math.random()*256).toString(16);
+        color1="#"+i+j+k;
+        return color1;
+}
 
+$('button').bind('click',function(){
+        var cir = output.append('circle');
+        
+        cir.attr('value',0).attr('cx', Math.floor(Math.random()*400)+100).attr('cy',Math.floor(Math.random()*400)+100).attr('r',(Math.random()*100)).attr('stroke', 'black').attr('fill', randomColor()).attr('storke-width', '2');
+    
+     var $cir = $('circle');
+    $cir.bind('click',function(){
+    var val = $(this).attr('value');
+    val = (val * 1 + 1) % 4;
+    $(this).attr('fill', randomColor()).attr('value', val);
+
+})
+})
+
+   
