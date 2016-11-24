98ea39981fae22d4baf8d7a7db06fa702f456a32
scoreace <478872763@qq.com>
2016-11-24 15:44:21 +0800

    20161124

--- zlx/20161121/http.js
+++ zlx/20161121/http.js
@@ -1,12 +1,27 @@
 var http = require('http');
-var server =http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.write('Halo Shijie\n');
-    res.write('这是个不简单的HTTP Server\n');
-    var a=345;
-    res.write(a.toString());
-    res.end();
+var server = http.createServer(function(req, res) {
+
+
+
+    // 引用 File System 模組
+    var fs = require('fs');
+
+    console.log(__dirname);
+
+    var filename = __dirname + '\\web\\index.html';
+    console.log(filename);
+
+    // 讀取檔案
+    fs.readFile(filename, 'utf8', function(err, content) {
+        if (err) {
+            console.log('Failed to read');
+            return;
+        }
+
+        // 將檔案內容印出
+        console.log(content);
+    });
 });
 
-server.listen(23333);
-console.log('Server running at http://120.125.63.159:23333/');
+server.listen(23456);
+console.log('Server running at http://192.168.0.105:23456/');
